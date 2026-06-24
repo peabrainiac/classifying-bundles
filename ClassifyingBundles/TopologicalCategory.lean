4cfd83afe61c1f6703121c8ccf0f6cbf6105ad73
Ben Eltschig <b.eltschig@protonmail.com>
2026-06-24 15:28:52 +0200

    feat: the nerve of a topological category

--- ClassifyingBundles/TopologicalCategory.lean
+++ ClassifyingBundles/TopologicalCategory.lean
@@ -3,10 +3,12 @@ Copyright (c) 2026 Ben Eltschig. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ben Eltschig
 -/
+import Mathlib.AlgebraicTopology.SimplicialObject.Basic
 import Mathlib.CategoryTheory.ComposableArrows.Basic
 import Mathlib.CategoryTheory.SingleObj
 import Mathlib.Tactic.IntervalCases
 import Mathlib.Topology.Algebra.Group.Defs
+import Mathlib.Topology.Category.TopCat.Basic
 import Mathlib.Topology.Homeomorph.TransferInstance
 
 /-! # Topological categories and groupoids
@@ -24,6 +26,7 @@ here instead of working abstractly with category/groupoid objects in `TopCat`.
   composition maps of `C` are continuous with respect to given topologies on `C` and `Arrow C`.
 * `IsTopologicalGroupoid C`: `Prop`-valued typeclass stating that a category is both a groupoid and
   a topological category for which the inverse map `Arrow C → Arrow C` is also continuous.
+* `topologicalNerve C`: the nerve of a topological category as a simplicial topological space.
 * For every topological monoid `M`, `SingleObj M` is a topological category.
 * For every topological group `G`, `SingleObj G` is a topological groupoid.
 -/
@@ -176,6 +179,20 @@ lemma continuous_hom {C : Type*} [Category* C] [TopologicalSpace C] [Topological
     Continuous (fun F ↦ F.hom : ComposableArrows C n → Arrow C) :=
   continuous_map'
 
+lemma continuous_map {C : Type*} [Category* C] [TopologicalSpace C] [TopologicalSpace (Arrow C)]
+    [∀ X Y : C, TopologicalSpace (X ⟶ Y)] [IsTopologicalCategory C]
+    {n : ℕ} {i j} {f : i ⟶ j} : Continuous (fun F ↦ F.map f : ComposableArrows C n → Arrow C) := by
+  obtain ⟨i, hi⟩ := i
+  obtain ⟨j, hj⟩ := j
+  rw [Subsingleton.elim f (homOfLE (leOfHom f))]
+  exact continuous_map' (h := leOfHom f)
+
+lemma continuous_whiskerLeft {C : Type*} [Category* C] [TopologicalSpace C]
+    [TopologicalSpace (Arrow C)] [∀ X Y : C, TopologicalSpace (X ⟶ Y)] [IsTopologicalCategory C]
+    {n m : ℕ} (Φ : Fin (n + 1) ⥤ Fin (m + 1)) :
+    Continuous (fun F : ComposableArrows C m ↦ F.whiskerLeft Φ) :=
+  continuous_iff.2 ⟨fun i hi ↦ continuous_obj', fun i hi ↦ continuous_map⟩
+
 end ComposableArrows
 
 /-- We say that a category is a topological groupoid if it is both a groupoid and a topological
@@ -254,5 +271,24 @@ instance {G : Type*} [Group G] [TopologicalSpace G] [IsTopologicalGroup G] :
     simp [Equiv.homeomorph, SingleObj.inv_as_inv]
 
 end Examples
+
+section Nerve
+
+open ConcreteCategory in
+/-- The *topological nerve* of a topological category is the simplicial topological space whose
+underlying simplicial set is the nerve of the underlying category, equipped with the appropriate
+topologies. -/
+@[simps -isSimp]
+def topologicalNerve (C : Type*) [Category* C] [TopologicalSpace C] [TopologicalSpace (Arrow C)]
+    [∀ X Y : C, TopologicalSpace (X ⟶ Y)] [IsTopologicalCategory C] : SimplicialObject TopCat where
+  obj Δ := .of (ComposableArrows C (Δ.unop.len))
+  map f := ofHom ⟨↾fun x ↦ x.whiskerLeft (SimplexCategory.toCat.map f.unop).toFunctor,
+    ComposableArrows.continuous_whiskerLeft _⟩
+  map_id _ := rfl
+  map_comp _ _ := rfl
+
+attribute [simp] topologicalNerve_obj_carrier
+
+end Nerve
 
 end CategoryTheory
